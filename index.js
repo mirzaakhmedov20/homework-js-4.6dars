3ef4fe7d4b1a63e616adc7f2c1507b84fa245372
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2023-12-19 21:12:31 +0500

    example work

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 // Javascriptda masalalar ishlash.
 // 3ta sondan kattasini topuvchi funksiya yozish.
-function kattaSon(num1, num2, num3) {
-  let number = Math.max(num1, num2, num3);
-  return number;
-}
-console.log(kattaSon(26, 25, 15));
+// function kattaSon(num1, num2, num3) {
+//   let number = Math.max(num1, num2, num3);
+//   return number;
+// }
+// console.log(kattaSon(26, 25, 15));
 
 // 2. Raqamlar bilan berilgan ixtiyoriy 2 xonali sonni so'zlar bilan ifodalab beruvchi funksiya:
+// function sozBn(son) {
+//   let birliklar = [
+//     "",
+//     "bir",
+//     "ikki",
+//     "uch",
+//     "to'rt",
+//     "besh",
+//     "olti",
+//     "yetti",
+//     "sakkiz",
+//     "to'qqiz",
+//   ];
+//   let onliklar = [
+//     "",
+//     "o'n",
+//     "yigirma",
+//     "o'ttiz",
+//     "qirq",
+//     "ellik",
+//     "oltmish",
+//     "yetmish",
+//     "sakson",
+//     "to'qson",
+//   ];
+
+//   let birlik = son % 10;
+//   let onlik = Math.floor(son / 10);
+
+//   let soz = "";
+
+//   switch (onlik) {
+//     case 0:
+//       soz += "";
+//       break;
+//     case 1:
+//       soz += "o'n ";
+//       break;
+//     default:
+//       soz += onliklar[onlik] + " ";
+//   }
+
+//   soz += birliklar[birlik];
+
+//   return soz;
+// }
+// console.log(sozBn(10));
 
 //3. 1 dan n gacha nechta toq son borligini aniqlovchi funksiya:
-function countOddNumbers(n) {
-  let count = 0;
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 !== 0) {
-      count++;
-    }
-  }
-  return count;
-}
+// function countOddNumbers(n) {
+//   let count = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 !== 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 // 4. Ixtiyoriy sonning bo'luvchilar sonini topuvchi funksiya yozing
-
-
-// 5 . Agar berilgan son 3 ga hamda 5 ga karrali bolsa uning kvadratini aks holda kubini qaytaruvchi fuksiya yozing.
-
-
-//6 . Ixtiyoriy 2 ta sondan raqamlari ko'paytmasi eng katta boladigani topib beruvchi funksiya yozing
-
-
-// 7 . Ixtiyoriy sonning tub yoki tub emasligini aniqlovchi funksiya yozing
+// function boluvchiSoni(number) {
+//   let count = 0;
+//   for (let i = 1; i <= number; i++) {
+//     if (number % i == 0) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+// let result = 20;
+// console.log(boluvchiSoni(result));
